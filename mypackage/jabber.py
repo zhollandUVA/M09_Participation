64863c3f7f23df5decae4b419f7637ceafdce88d
Zach <nms9dg@virginia.edu>
2024-03-26 20:22:43 -0400

    Part 9  for DS5100

---
+++ mypackage/jabber.py
@@ -0,0 +1,8 @@
+def jabber():
+    """
+    This function prints a welcome message.
+    """
+    print("Welcome to my Python package!")
+
+if __name__ == "__main__":
+    jabber()
---
+++ mypackage/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/ontoligent/m09-demo',
+    author='Zachary Holland',
+    author_email='nms9dg@virginia.edu',
+    license='MIT',
+    packages=['demo']
+)
